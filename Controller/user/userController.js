421e1766657b4faff8bd9aec64bedb51604a0329
Navyacs-crypto <navyacshaji12@gmail.com>
2025-05-05 21:50:29 +0530

    Otp generated through nodemailer

--- Controller/user/userController.js
+++ Controller/user/userController.js
@@ -1,16 +1,18 @@
-const User=require("../../Models/userSchema")
+const User = require("../../Models/userSchema")
+const nodemailer = require('nodemailer')
+const env = require('dotenv').config()
 
-const LoadHomePage=async(req,res)=>{
+const LoadHomePage = async (req, res) => {
 
-try {
-   return  res.render("Home")
-} catch (error) {
-    console.log(error)
-    res.status(500).send("server error")
+    try {
+        return res.render("Home")
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("server error")
+    }
 }
-}
 
-const LoadSignupPage=async(req,res)=>{
+const LoadSignupPage = async (req, res) => {
     try {
         return res.render("signup")
     } catch (error) {
@@ -19,21 +21,64 @@ const LoadSignupPage=async(req,res)=>{
     }
 }
 
-const signUp=async(req,res)=>{
-    const {name,email,phone,password}=req.body
+
+function generateOtp() {
+    return Math.floor(100000 + Math.random() * 900000).toString()
+}
+async function sendVerificationEmail(email, otp) {
     try {
-        const newUser=new User({name,email,phone,password})
-         await newUser.save()
-         console.log(newUser)
-        return res.redirect("/signup")
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            port: 587,
+            secure: false,
+            requireTLs: true,
+            auth: {
+                user: process.env.NODEMAILER_EMAIL,
+                pass: process.env.NODEMAILER_PASSWORD
+            }
+        })
+
+        const info = await transporter.sendMail({
+            from: process.env.NODEMAILER_EMAIL,
+            to: email,
+            subject: "verify your account",
+            text: `your OTP is ${otp}`,
+            html: `<b>Your OTP :${otp} </b>`,
+        })
+        return info.accepted.length > 0
+
     } catch (error) {
-        console.error(error)
-        res.status(500).send(" Internal Server error")
+        console.error("Error sending email", error);
+        return false;
+    }
+}
+const signUp = async (req, res) => {
+    try {
+
+        const { email, password, confirmpassword } = req.body
+        if (password !== confirmpassword) {
+            res.render('signup', { messege: "Passwords are not match" })
+        }
+        const findUser = await User.findOne({ email: req.body.email });
+        if (findUser) {
+            res.render('signup', { messege: "User already exist" })
+        }
+        const otp = generateOtp()
+        const emailSent = await sendVerificationEmail(email, otp)
+        if (!emailSent) {
+            return res.json('email-error')
+        }
+        req.session.userOtp = otp;
+        req.session.userdata = { email, password }
+        // res.render("verify-otp")
+        console.log("OTP", otp)
+    } catch (error) {
+        console.error("Signup error", error)
+        res.redirect("/pageNotFound")
     }
 }
 
-
-const PageNotFound=async(req,res)=>{
+const PageNotFound = async (req, res) => {
     try {
         res.render("PageNotFound");
     } catch (error) {
@@ -41,7 +86,7 @@ const PageNotFound=async(req,res)=>{
     }
 }
 
-module.exports={
+module.exports = {
     LoadHomePage,
     PageNotFound,
     LoadSignupPage,
--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-const express=require('express')
-const app=express()
-const Dotenv=require('dotenv').config()
-const db=require('./Config/db')
-const path=require('path')
-const userRouter=require('./Routes/userRouter')
+const express = require('express')
+const app = express()
+const Dotenv = require('dotenv').config()
+const db = require('./Config/db')
+const path = require('path')
+const userRouter = require('./Routes/userRouter')
+const session = require('express-session')
+
 db();
+
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        httpOnly: true,
+        maxAge: 72 * 60 * 60 * 1000
+    }
+}))
+
+app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-store');
+    next();
+})
 
 //Middlewares....
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 //ViewEngine.........
 
-app.set("view engine","ejs")
-app.set("views", [path.join(__dirname,"views/user"),path.join(__dirname,"view/Admin")])
+app.set("view engine", "ejs")
+app.set("views", [path.join(__dirname, "views/user"), path.join(__dirname, "view/Admin")])
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes.........
-app.use("/",userRouter)
+app.use("/", userRouter)
 
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
     console.log("server is running  ")
 })
 
 
 
-module.exports=app
+module.exports = app
